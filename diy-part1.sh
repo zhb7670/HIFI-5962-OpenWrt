733d8272dca228eabec335fbcafd95aec8fc036a
zhb7670 <52443818+zhb7670@users.noreply.github.com>
2021-11-23 10:58:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,6 @@
 # Add a feed source
 sed -i '$a src-git packages https://github.com/coolsnowwolf/packages' feeds.conf.default
 sed -i '$a src-git luci https://github.com/xiaowansm5/luci' feeds.conf.default
-sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git' feeds.conf.default
-sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default 
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
